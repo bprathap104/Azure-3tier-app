1d832d312d9e0412c0b67860c24e3295da9b1ece
Omurbek Orozaliev <99226984+Oma1993@users.noreply.github.com>
2022-07-29 10:21:48 -0500

    apdated delete option

--- main.tf
+++ main.tf
@@ -25,31 +25,6 @@ resource "azurerm_network_security_group" "Team2" {
   location            = azurerm_resource_group.Team2.location
   resource_group_name = azurerm_resource_group.Team2.name
 }
-# module "vnet" {
-#   source              = "Azure/vnet/azurerm"
-#   resource_group_name = azurerm_resource_group.Team2.name
-#   address_space       = ["10.0.0.0/16"]
-#   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   subnet_names        = ["subnet1", "subnet2", "subnet3"]
-#   depends_on          = [azurerm_resource_group.Team2]
-
-
-  # subnet_service_endpoints = {
-  #   subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
-  #   subnet3 = ["Microsoft.AzureActiveDirectory"]
-  # }
-
-  # tags = {
-  #   name        = "Team2"
-  #   environment = "dev"
-  #   costcenter  = "it"
-  # }
-#}
-
-# resource "azurerm_resource_group" "Team2" {
-#   name     = "my-resources"
-#   location = "East US"
-# }
 
 resource "azurerm_virtual_network" "Team2" {
   name                = "my-network"
@@ -87,7 +62,7 @@ resource "azurerm_virtual_machine" "Team2" {
   vm_size               = "Standard_B1s"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+   delete_os_disk_on_termination = true
 
   # Uncomment this line to delete the data disks automatically when deleting the VM
   # delete_data_disks_on_termination = true
